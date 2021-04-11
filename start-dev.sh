b1a8300c37d09f04b10d783d394b65ce10f98f07
sachinAthu <sachin2262716@gmail.com>
2021-04-12 03:43:40 +0530

    first commit

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-# start docker compose
-docker-compose up --build
+# for development
+# use NodePort insted of using CulterIP
+# start all k8 deplyments, services, secrets, presistance storages
+kubectl apply -f ./k8s
