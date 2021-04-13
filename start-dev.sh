e265b718ddc4256f7325230e07ad854a1e155a14
sachinAthu <sachin2262716@gmail.com>
2021-04-13 15:58:14 +0530

    add conf for book customer order

--- start-dev.sh
+++ start-dev.sh
@@ -2,5 +2,10 @@
 
 # for development
 # use NodePort insted of using CulterIP
-# start all k8 deplyments, services, secrets, presistance storages
-kubectl apply -f ./k8s
+
+# create secrets for mongo username and password
+kubectl create secret generic mongousername --from-literal MONGO_USERNAME=mongouser
+kubectl create secret generic mongopassword --from-literal MONGO_PASSWORD=12345
+
+# start all k8 deplyments, services, secrets, presistance storage
+#kubectl apply -f ./k8s
