b77f42d26abce479abc783c211dfd46ad1529207
sachinAthu <sachin2262716@gmail.com>
2021-04-13 22:16:28 +0530

    mysql auth added. node:alpine used

--- Books/books.js
+++ Books/books.js
@@ -1,102 +1,89 @@
-const keys = require('./keys')
+const keys = require("./keys");
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 
 // const mongoURI = 'mongodb+srv://rp:rp123@cluster0.i9xwy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const mongoURI = `mongodb://${keys.mongoHost}:${keys.mongoPort}/library_books`
+const mongoURI = `mongodb://${keys.mongoHost}:${keys.mongoPort}/${keys.mongoDatabase}`;
 
 mongoose
-    .connect(mongoURI,{useNewUrlParser: true , useUnifiedTopology :  true})
-    .then(() => console.log('MongoDB is Connected'))
-    .catch(err => console.log(err))
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    auth: {
+      user: keys.mongoUsername,
+      password: keys.mongoPassword,
+    },
+  })
+  .then(() => console.log("MongoDB is Connected"))
+  .catch((err) => console.log(err));
 
+const Book = require("./Book");
+const { mongoPort } = require("./keys");
 
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
 
-const Book = require('./Book');
-const { mongoPort } = require('./keys');
-
-
-app.get('/books',async (req,res)=>{
-
-    try {
-        const books = await Book.find({})
-
-        if(books){
-            res.json(books);
-        }else{
-            res.status(404).json({message:'Books Empty'});
-        }
+    if (books) {
+      res.json(books);
+    } else {
+      res.status(404).json({ message: "Books Empty" });
     }
-    catch(err){
-        res.json({error:err})
-    }
-
-})
-
-
-
-app.get('/books/:id',async (req,res)=>{
-
-    try {
-        const book = await Book.findById({_id:req.params.id})
-
-        if(book){
-            res.json(book);
-        }else{
-            res.status(404).json({message:'No such Book Found'});
-        }
-    }
-    catch(err){
-        res.json({error:err})
-    }
-    
-})
-
-
-
-app.post("/books",async (req,res)=>{
-    let newBook = {
-        title:req.body.title,
-        author:req.body.author,
-        numberPages:req.body.numberPages,
-        publisher:req.body.publisher
-    }
-    try {
-
-        let result  = await Book.create(newBook); 
-        res.status(200).json(result);
- 
-    } catch (err) {
-        res.status(400).json({
-            message: err.message,
-        });
-    }
-
-})
-
-
-
-app.delete("/books/:id",async (req,res)=>{
-    try {
-        const book = await Book.findByIdAndDelete(req.params.id);
-
-        if(book){
-            res.status(200).json(book);
-        }else{
-            res.status(404).json({'msg':'No such food'})
-        }
-    } catch (err) {
-        res.status(500).json({error:'Server Error'})
-    }
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 
+app.get("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findById({ _id: req.params.id });
 
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ message: "No such Book Found" });
+    }
+  } catch (err) {
+    res.json({ error: err });
+  }
+});
 
-app.listen(4545,()=>{
-    console.log('Book Service start on PORT 4545')
-})
+app.post("/books", async (req, res) => {
+  let newBook = {
+    title: req.body.title,
+    author: req.body.author,
+    numberPages: req.body.numberPages,
+    publisher: req.body.publisher,
+  };
+  try {
+    let result = await Book.create(newBook);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({ msg: "No such food" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+app.listen(4545, () => {
+  console.log("Book Service start on PORT 4545");
+});
--- Customers/keys.js
+++ Customers/keys.js
@@ -2,5 +2,6 @@ module.exports = {
     mongoUsername: process.env.MONGO_USERNAME,
     mongoPassword: process.env.MONGO_PASSWORD,
     mongoHost: process.env.MONGO_HOST,
-    mongoPort: process.env.MONGO_PORT
+    mongoPort: process.env.MONGO_PORT,
+    mongoDatabase: process.env.MONGO_DATABASE
 }
